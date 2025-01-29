83cbe2fc8f7fff5421437084914550d299f63936
zywelf <zywelf@gmail.com>
2025-01-29 19:38:27 +0100

    Sistemazione sezione esperienza e piccole migliorie aggiunte

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Comando per aprire il menu navigazione da telefono
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
-
+// Funzione per attivare il menu comparsa da telefono
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
@@ -9,24 +9,14 @@ menuIcon.onclick = () => {
 //Comandi per cambiare il tema del portfolio
 const themeSwitch = document.getElementById('theme-switch');
 const themeIcon = document.getElementById('theme-icon');
-const root = document.documentElement; // Selettore :root per gestire le variabili CSS
+const root = document.documentElement; // Selettore root per gestire le variabili CSS
 
-// Funzione per alternare il tema
+// Funzione per alternare il tema chiaro e scuro del portfolio
 themeSwitch.addEventListener('click', () => {
-    // Alterna la classe 'light-mode' al root
     root.classList.toggle('light-mode');
-
-    // // Cambia l'icona
-    // if (root.classList.contains('light-mode')) {
-    //     themeIcon.classList.remove('fa-lightbulb');
-    //     themeIcon.classList.add('fa-solid', 'fa-lightbulb'); // Icona accesa
-    // } else {
-    //     themeIcon.classList.remove('fa-solid', 'fa-lightbulb');
-    //     themeIcon.classList.add('fa-lightbulb'); // Icona spenta
-    // }
 });
 
-// Comando per apparizione popup
+// Funzione per apertura e chiusura popup
 function showPopup() {
     document.getElementById("popup").classList.remove("hidden");
 }
